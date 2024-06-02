d41ebd6f857cad763a6f974a26a6a53cd0b4c466
zeenat <zeenu.23@gmail.com>
2024-06-02 18:34:55 +0530

    update CSS styles, formatting, and minor code refactoring in SignInPage and SignUpPage components

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
 import { LockOutlined } from "@mui/icons-material";
-import GoogleIcon from '@mui/icons-material/Google';
+import GoogleIcon from "@mui/icons-material/Google";
 import {
   Container,
   CssBaseline,
@@ -12,9 +12,10 @@ import {
   Button,
   Grid,
 } from "@mui/material";
-import { authenticate } from '../utils/auth';
+
+import { authenticate } from "../utils/auth";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import '../globalStyles.css'
+import "../globalStyles.css";
 
 interface SignInProps {}
 interface Errors {
@@ -23,28 +24,30 @@ interface Errors {
 }
 
 const SignInPage: React.FC<SignInProps> = () => {
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
-  const [errors, setErrors] = useState<Errors>({ emailError: '', passwordError: '' });
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [errors, setErrors] = useState<Errors>({
+    emailError: "",
+    passwordError: "",
+  });
 
   const navigate = useNavigate();
-
   const provider = new GoogleAuthProvider();
   const auth = getAuth();
 
   const validateFields = () => {
-  if (!email.trim() ||!password.trim()) {
-    if (!email.trim()) {
-      setErrors({...errors, emailError: 'Email is required.' });
+    if (!email.trim() || !password.trim()) {
+      if (!email.trim()) {
+        setErrors({ ...errors, emailError: "Email is required." });
+      }
+      if (!password.trim()) {
+        setErrors({ ...errors, passwordError: "Password is required." });
+      }
+      return false;
     }
-    if (!password.trim()) {
-      setErrors({...errors, passwordError: 'Password is required.' });
-    }
-    return false;
-  }
-  setErrors({ emailError: '', passwordError: '' });
-  return true;
-};
+    setErrors({ emailError: "", passwordError: "" });
+    return true;
+  };
 
   const handleSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
@@ -52,113 +55,138 @@ const SignInPage: React.FC<SignInProps> = () => {
       return;
     }
     authenticate({ email });
-    navigate('/dashboard');
+    navigate("/dashboard");
   };
 
-const handleGoogleSignIn = (): void => {
-  signInWithPopup(auth, provider)
-    .then((result: any) => {
-      const idToken: string | undefined = result.user?.accessToken;
-      if (idToken) {
-        console.log(idToken);
-        authenticate({ idToken });
-        navigate('/dashboard');
-      } else {
-        console.error('ID token is null.');
-      }
-    })
-    .catch((error: any) => {
-      const errorMessage: string = error.message;
-      console.error(errorMessage);
-    });
-};
+  const handleGoogleSignIn = (): void => {
+    signInWithPopup(auth, provider)
+      .then((result: any) => {
+        const idToken: string | undefined = result.user?.accessToken;
+        if (idToken) {
+          console.log(idToken);
+          authenticate({ idToken });
+          navigate("/dashboard");
+        } else {
+          console.error("ID token is null.");
+        }
+      })
+      .catch((error: any) => {
+        const errorMessage: string = error.message;
+        console.error(errorMessage);
+      });
+  };
 
   return (
     <>
       <div>
-      <div className="overlayGradient" >
-      <Container maxWidth="xs" >
-        <CssBaseline />
-        <Box
-          className="glassmorphicBackground"
-          sx={{
-            mt: 20,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: "primary.light" }}>
-            <LockOutlined />
-          </Avatar>
-          <Typography variant="h5">Login</Typography>
-          <Box sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoFocus
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              error={!!errors.emailError}
-              helperText={errors.emailError}
-            />
-
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="password"
-              name="password"
-              label="Password"
-              type="password"
-              value={password}
-              onChange={(e) => {
-              setPassword(e.target.value);
-                  }}
-              error={!!errors.passwordError}
-              helperText={errors.passwordError}
-            />
-
-            <Button
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-              onClick={handleSubmit}
+        <div className="overlayGradient">
+          <Container maxWidth="xs">
+            <CssBaseline />
+            <Box
+              className="glassmorphicBackground"
+              sx={{
+                mt: 20,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
             >
-              Sign In
-          </Button>
-                
-        <Button
-            fullWidth
-            variant="contained"
-            startIcon={<GoogleIcon />}
-            onClick={handleGoogleSignIn}
-            sx={{
-              backgroundColor: '#d34836',
-              color: '#fff',
-              '&:hover': {
-              backgroundColor: '#3367D6',
-             },
-            }}
-           >
-           Sign in with Google
-        </Button>
-                
-            <Grid container justifyContent={"flex-end"}>
-              <Grid item>
-                <Box sx={{ pt: 2, pb: 2, pr: 2, pl: 2 }}>
-                 <Link to="/signup">Don't have an account? Signup</Link>
-                </Box>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-      </Container>
-      </div>
+              <Avatar sx={{ m: 1, bgcolor: "primary.light" }}>
+                <LockOutlined />
+              </Avatar>
+              <Typography variant="h5" sx={{ color: "white" }}>
+                Login
+              </Typography>
+              <Box sx={{ mt: 1 }}>
+                <Grid container spacing={2} alignItems="center">
+                  <Grid item xs={12}>
+                    <TextField
+                      fullWidth
+                      margin="normal"
+                      required
+                      id="email"
+                      label="Email Address"
+                      name="email"
+                      autoFocus
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      error={!!errors.emailError}
+                      helperText={errors.emailError}
+                      sx={{
+                        ".MuiInputLabel-root": {
+                          color: "white",
+                          "&.Mui-focused": {
+                            color: "white",
+                          },
+                        },
+                      }}
+                    />
+                  </Grid>
+                </Grid>
+
+                <Grid container spacing={2} alignItems="center">
+                  <Grid item xs={12}>
+                    <TextField
+                      fullWidth
+                      margin="normal"
+                      required
+                      id="password"
+                      name="password"
+                      label="Password"
+                      type="password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      error={!!errors.passwordError}
+                      helperText={errors.passwordError}
+                      sx={{
+                        ".MuiInputLabel-root": {
+                          color: "white",
+                          "&.Mui-focused": {
+                            color: "white",
+                          },
+                        },
+                      }}
+                    />
+                  </Grid>
+                </Grid>
+                <Button
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                  onClick={handleSubmit}
+                >
+                  Sign In
+                </Button>
+
+                <Button
+                  fullWidth
+                  variant="contained"
+                  startIcon={<GoogleIcon />}
+                  onClick={handleGoogleSignIn}
+                  sx={{
+                    backgroundColor: "#d34836",
+                    color: "#fff",
+                    "&:hover": {
+                      backgroundColor: "#57130a",
+                    },
+                  }}
+                >
+                  Sign in with Google
+                </Button>
+
+                <Grid container justifyContent={"flex-end"}>
+                  <Grid item>
+                    <Box sx={{ pt: 2, pb: 2, pr: 2, pl: 2 }}>
+                      <Link to="/signup" style={{ color: "white" }}>
+                        Don't have an account? Signup
+                      </Link>
+                    </Box>
+                  </Grid>
+                </Grid>
+              </Box>
+            </Box>
+          </Container>
+        </div>
       </div>
     </>
   );
--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import {
   Container,
   CssBaseline,
@@ -8,107 +8,135 @@ import {
   Typography,
   TextField,
   Button,
-  Alert
+  Alert,
 } from "@mui/material";
-import { LockOutlined } from '@mui/icons-material';
-import { signup, isAuthenticated } from '../utils/auth';
+import { LockOutlined } from "@mui/icons-material";
+import { signup } from "../utils/auth";
 
 interface SignUpProps {}
 
 const SignUpPage: React.FC<SignUpProps> = () => {
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
-  const [confirmPassword, setConfirmPassword] = useState<string>('');
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [confirmPassword, setConfirmPassword] = useState<string>("");
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const handleSubmit = (userData: any) => {
     signup(userData).then((success: any) => {
       if (success) {
         setShowSuccessMessage(true);
         setTimeout(() => {
-          navigate('/'); 
+          navigate("/");
         }, 2000);
       }
     });
   };
 
- 
   return (
     <>
       <div className="signInPageBackground">
-      <div className="overlayGradient">
-      <Container maxWidth="xs">
-        <CssBaseline />
-        <Box
-          className="glassmorphicBackground"
-          sx={{
-            mt: 20,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: "primary.light" }}>
-            <LockOutlined />
-          </Avatar>
-          <Typography variant="h5">Signup</Typography>
-          <Box sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoFocus
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="password"
-              name="password"
-              label="Password"
-              type="password"
-              value={password}
-              onChange={(e) => {
-                setPassword(e.target.value);
+        <div className="overlayGradient">
+          <Container maxWidth="xs">
+            <CssBaseline />
+            <Box
+              className="glassmorphicBackground"
+              sx={{
+                mt: 20,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
               }}
-            />               
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="confirmPassword"
-              name="confirmPassword"
-              label="Confirm Password"
-              type="password"
-              value={confirmPassword}
-              onChange={(e) => {
-                setConfirmPassword(e.target.value);
-              }}
-            />                
-            <Button
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-              onClick={handleSubmit}
             >
-              Signup
-            </Button>
-          </Box>
-        </Box>
-      {showSuccessMessage && (
-        <Alert severity="success" onClose={() => setShowSuccessMessage(false)}>
-          Signup successful! Redirecting...
-        </Alert>
-      )}
-      </Container>
-      </div>
+              <Avatar sx={{ m: 1, bgcolor: "primary.light" }}>
+                <LockOutlined />
+              </Avatar>
+              <Typography variant="h5" sx={{ color: "white" }}>
+                Signup
+              </Typography>
+              <Box sx={{ mt: 1 }}>
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoFocus
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  sx={{
+                    ".MuiInputLabel-root": {
+                      color: "white",
+                      "&.Mui-focused": {
+                        color: "white",
+                      },
+                    },
+                  }}
+                />
+
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="password"
+                  name="password"
+                  label="Password"
+                  type="password"
+                  value={password}
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
+                  sx={{
+                    ".MuiInputLabel-root": {
+                      color: "white",
+                      "&.Mui-focused": {
+                        color: "white",
+                      },
+                    },
+                  }}
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="confirmPassword"
+                  name="confirmPassword"
+                  label="Confirm Password"
+                  type="password"
+                  value={confirmPassword}
+                  onChange={(e) => {
+                    setConfirmPassword(e.target.value);
+                  }}
+                  sx={{
+                    ".MuiInputLabel-root": {
+                      color: "white",
+                      "&.Mui-focused": {
+                        color: "white",
+                      },
+                    },
+                  }}
+                />
+                <Button
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                  onClick={handleSubmit}
+                >
+                  Signup
+                </Button>
+              </Box>
+            </Box>
+            {showSuccessMessage && (
+              <Alert
+                severity="success"
+                onClose={() => setShowSuccessMessage(false)}
+              >
+                Signup successful! Redirecting...
+              </Alert>
+            )}
+          </Container>
+        </div>
       </div>
     </>
   );
